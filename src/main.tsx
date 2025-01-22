7a5a83d0c46fda334249dd7e9dfecd9a411cd9e2
Baltukov Nomto <disma785@gmail.com>
2025-01-23 00:06:04 +0800

    d

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import { TonConnectUIProvider } from "@tonconnect/ui-react";
 // (window as any).Buffer = Buffer;
 
 createRoot(document.getElementById("root")!).render(
-  <TonConnectUIProvider manifestUrl="https://teal-big-falcon-529.mypinata.cloud/files/bafkreid5vqslewk7mjpwoyjx64djwoepzddavrjuslsdhyvbirioq53vi4?X-Algorithm=PINATA1&X-Date=1737561775&X-Expires=30&X-Method=GET&X-Signature=a62f73fd941e4d2a63cafb00906197be2b3c5c5bbe680fbe9870e052304e743a">
+  <TonConnectUIProvider manifestUrl="https://teal-big-falcon-529.mypinata.cloud/files/bafkreiczltcgyz24xfddatblru3fujxf74sw7at44dnsvdv37mwa2mnb2e?X-Algorithm=PINATA1&X-Date=1737561920&X-Expires=30&X-Method=GET&X-Signature=fbe26475133c6dc3174dc4c2ce06fe931f98fcf495e64a07800e39a239dfc1d7">
     <App />
   </TonConnectUIProvider>
 );
