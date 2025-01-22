9bd9c874c6addd457747aedeca2f208caef33a95
Baltukov Nomto <disma785@gmail.com>
2025-01-22 15:38:17 +0800

    fefew

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,13 @@
 import ReactDOM from "react-dom/client";
 import "./index.scss";
 import App from "./App";
+import { TonConnectUIProvider } from "@tonconnect/ui-react";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(<App />);
+root.render(
+  <TonConnectUIProvider manifestUrl="https://violet-traditional-rabbit-103.mypinata.cloud/ipfs/QmQJJAdZ2qSwdepvb5evJq7soEBueFenHLX3PoM6tiBffm">
+    <App />
+  </TonConnectUIProvider>
+);
