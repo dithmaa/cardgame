f6c4fb71ed5ab2d59329ef0c2ee794a327217344
Baltukov Nomto <disma785@gmail.com>
2025-02-03 02:07:46 +0800

    narana, i wanna meet u

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import { TonConnectUIProvider } from "@tonconnect/ui-react";
 // (window as any).Buffer = Buffer;
 
 createRoot(document.getElementById("root")!).render(
-  <TonConnectUIProvider manifestUrl="https://teal-big-falcon-529.mypinata.cloud/files/bafkreiayhequh5wlinfj2nq7apdm5pzyxe2ss7dy4agzvmyc5hr5vu2t6m?X-Algorithm=PINATA1&X-Date=1738516918&X-Expires=30&X-Method=GET&X-Signature=e96b6605928c613587012a7b13964a8c2db8135b69a7811e63e9774485115d26">
+  <TonConnectUIProvider manifestUrl="https://raw.githubusercontent.com/dithmaa/cardgame/main/mainfest.json">
     <App />
   </TonConnectUIProvider>
 );
